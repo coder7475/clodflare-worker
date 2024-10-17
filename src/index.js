f84a8dc67a82a28c876148ba2f757114260e01b3
coder7475 <robiulhossain7475@gmail.com>
2024-10-17 16:32:45 +0600

    feat: try hono

--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
-/**
- * Cloudflare Worker Webhook
- *
- * This worker creates a simple webhook that can receive POST requests
- * and respond to them.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Send a POST request to http://localhost:8787/webhook to test
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
 
-export default {
-	async fetch(request, env, ctx) {
-		return new Response(JSON.stringify({ message: 'Hello World' }), {
-			headers: { 'Content-Type': 'application/json' },
-		});
-	},
-};
+const app = new Hono();
+
+app.get('/', (c) => c.json({ message: 'Hello World' }));
+
+export default app;
